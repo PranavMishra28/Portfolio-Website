3bbe7e05dbb4226ab0b663a4680bd6d6d54285e0
Pranav Mishra <mishrapranav82@gmail.com>
2024-11-17 22:34:21 -0500

    new footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-darkgray text-white py-4 sm:py-6">
       <div className="container mx-auto text-center">
-        <div className="font-semibold text-lg sm:text-2xl">Krush<span className="text-orange">i</span>l Amrut<span className="text-orange">i</span>ya</div>
+        <div className="font-semibold text-lg sm:text-2xl">Pranav Mishra</div>
         <div className="text-base mt-2">
           <a href="mailto:contact@krushil.dev">
             <i className="hover:text-orange bx bxs-envelope"></i>
