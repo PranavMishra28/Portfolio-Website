b02d880f424105af203acc151e353cb325db2345
Pranav Mishra <mishrapranav82@gmail.com>
2025-02-24 06:43:02 -0500

    vercell-analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { Analytics } from "@vercel/analytics/react"
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import ResumePage from "./components/ResumePage";
 import Footer from "./components/Footer";
